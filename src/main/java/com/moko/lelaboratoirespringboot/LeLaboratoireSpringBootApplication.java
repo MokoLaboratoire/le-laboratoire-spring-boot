5b3493a4c5f01b86812006c173ce6b8d0729f889
MokoLaboratoire <desmoulin.federico@tutanota.com>
2023-11-14 13:07:06 +0000

    Spring Initializr

---
+++ src/main/java/com/moko/lelaboratoirespringboot/LeLaboratoireSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.moko.lelaboratoirespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeLaboratoireSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeLaboratoireSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moko/lelaboratoirespringboot/LeLaboratoireSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moko.lelaboratoirespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeLaboratoireSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
